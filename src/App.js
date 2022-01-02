3c4373adb6c716a86cd09f2549938ab9e994b7ee
Amar Gandhi <amargandhi10@gmail.com>
2022-01-02 04:22:28 -0800

    Add image browsing with image number

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import React from 'react';
 import ButtonView from './components/ButtonView';
 import TextInput from './components/TextInput';
+import jsonData from './data/split2.json'
 
 class App extends React.Component {
     constructor(props) {
@@ -12,16 +13,26 @@ class App extends React.Component {
     }
 
     handleIdChange(idValue) {
-        this.setState({ id: idValue })
+        const idNum = parseInt(idValue, 10);
+        if(idValue == null || idValue == undefined || isNaN(idNum)) {
+            this.setState({ id: 0 });
+        } else {
+            this.setState({ id: idNum });
+        }
     }
 
     render() {
         const imageId = this.state.id;
+
+        // Check if value exists before use
+        const imageFileName = !jsonData[imageId.toString()] ? "" : jsonData[imageId.toString()].name;
+
+        const imageUrl = "https://gitlab.com/acgandhi/handheld-classification-files/-/raw/main/" + imageFileName;
         return (
             <div>
                 hello world
                 <TextInput value={imageId} label="Image #:" onChange={this.handleIdChange}></TextInput>
-                <div>{this.state.id}</div>
+                <img src={imageUrl}/>
             </div>
             
         );
